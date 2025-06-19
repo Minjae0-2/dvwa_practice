c8ed468d4a53f9dbcbf47b6d423f584ed55e916b
Minjae0-2 <lawpea38@gmail.com>
2025-06-19 17:36:36 +0900

    command injection low레벨 취약점 실습

---
+++ modified-code/Command Injection.php
@@ -0,0 +1,21 @@
+<?php
+
+if( isset( $_POST[ 'Submit' ]  ) ) {
+        // Get input
+        $target = escapeshellcmd($_REQUEST[ 'ip' ]); // escapeshellcmd로 특수문자를 이스케이프 처리 
+
+        // Determine OS and execute the ping command.
+        if( stristr( php_uname( 's' ), 'Windows NT' ) ) {
+                // Windows
+                $cmd = shell_exec( 'ping  ' . $target );
+        }
+        else {
+                // *nix
+                $cmd = shell_exec( 'ping  -c 4 ' . $target );
+        }
+
+        // Feedback for the end user
+        $html .= "<pre>{$cmd}</pre>";
+}
+
+?>
