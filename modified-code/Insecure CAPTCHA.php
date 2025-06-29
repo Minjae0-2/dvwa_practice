cc4aec4d2121466898e608bb75e07f6780bcfdc6
Minjae0-2 <lawpea38@gmail.com>
2025-06-29 18:30:22 +0900

    Insecure CAPTCHA low레벨 실습

---
+++ modified-code/Insecure CAPTCHA.php
@@ -0,0 +1,90 @@
+<?php
+
+if( isset( $_POST[ 'Change' ] ) && ( $_POST[ 'step' ] == '1' ) ) {
+        // Hide the CAPTCHA form
+        $hide_form = true;
+
+        // Get input
+        $pass_new  = $_POST[ 'password_new' ];
+        $pass_conf = $_POST[ 'password_conf' ];
+
+        // Check CAPTCHA from 3rd party
+        $resp = recaptcha_check_answer(
+                $_DVWA[ 'recaptcha_private_key'],
+                $_POST['g-recaptcha-response']
+        );
+
+        // Did the CAPTCHA fail?
+        if( !$resp ) {
+                // What happens when the CAPTCHA was entered incorrectly
+                $html     .= "<pre><br />The CAPTCHA was incorrect. Please try again.</pre>";
+                $hide_form = false;
+                return;
+        }
+        else {
+                // CAPTCHA was correct. Do both new passwords match?
+                if( $pass_new == $pass_conf ) {
+                        
+                        //💡 CAPTCHA 인증 성공하면 세션에 기록.
+                        $_SESSION['captcha_pass']=true;
+                        
+                        // Show next stage for the user
+                        $html .= "
+                                <pre><br />You passed the CAPTCHA! Click the button to confirm your changes.<br /></pre>
+                                <form action=\"#\" method=\"POST\">
+                                        <input type=\"hidden\" name=\"step\" value=\"2\" />
+                                        <input type=\"hidden\" name=\"password_new\" value=\"{$pass_new}\" />
+                                        <input type=\"hidden\" name=\"password_conf\" value=\"{$pass_conf}\" />
+                                        <input type=\"submit\" name=\"Change\" value=\"Change\" />
+                                </form>";
+                }
+                else {
+                        // Both new passwords do not match.
+                        $html     .= "<pre>Both passwords must match.</pre>";
+                        $hide_form = false;
+                }
+        }
+}
+
+if( isset( $_POST[ 'Change' ] ) && ( $_POST[ 'step' ] == '2' ) ) {
+        // Hide the CAPTCHA form
+        $hide_form = true;
+
+        //💡 step=2 실행 전, step=1 통과했는지 세션 변수를 통해 확인
+        if(isset($_SESSION['captcha_pass']) && ($_SESSION['captcha_pass'])==true ){
+
+        // Get input
+        $pass_new  = $_POST[ 'password_new' ];
+        $pass_conf = $_POST[ 'password_conf' ];
+
+        // Check to see if both password match
+        if( $pass_new == $pass_conf ) {
+                // They do!
+                $pass_new = ((isset($GLOBALS["___mysqli_ston"]) && is_object($GLOBALS["___mysqli_ston"])) ? mysqli_real_escape_string($GLOBALS["___mysqli_ston"],  $pass_new ) : ((trigger_error("[MySQLConverterToo] Fix the mysql_escape_string() call! This code does not work.", E_USER_ERROR)) ? "" : ""));
+                $pass_new = md5( $pass_new );
+
+                // Update database
+                $insert = "UPDATE `users` SET password = '$pass_new' WHERE user = '" . dvwaCurrentUser() . "';";
+                $result = mysqli_query($GLOBALS["___mysqli_ston"],  $insert ) or die( '<pre>' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) . '</pre>' );
+
+                // Feedback for the end user
+                $html .= "<pre>Password Changed.</pre>";
+        }
+        else {
+                // Issue with the passwords matching
+                $html .= "<pre>Passwords did not match.</pre>";
+                $hide_form = false;
+        }
+        //💡재사용 방지 위해 step2 끝나면 세션 변수 삭제
+        unset($_SESSION['captcha_pass'])
+}else{
+        //💡세션 변수가 없으면 step=1 통과 못한 것이므로, 블락된다.
+        $html .= "<pre>Invalid request. Please complete CAPTCHA step 1.</pre>";
+        $hide_form = false;
+}
+
+        ((is_null($___mysqli_res = mysqli_close($GLOBALS["___mysqli_ston"]))) ? false : $___mysqli_res);
+}
+
+?>
+     
